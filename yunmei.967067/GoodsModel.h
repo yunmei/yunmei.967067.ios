32ef8934a96627baf1fb85ae75d08dea721d29e6
bevin1984 <bevin1984@gmail.com>
2012-11-29 15:15:12 +0800

    添加GoodsModel

---
+++ yunmei.967067/GoodsModel.h
@@ -0,0 +1,18 @@
+//
+//  GoodsModel.h
+//  yunmei.967067
+//
+//  Created by bevin chen on 12-11-29.
+//  Copyright (c) 2012年 bevin chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoodsModel : NSObject
+
+@property NSUInteger goodsId;
+@property(strong, nonatomic)NSString *goodsPrice;
+@property(strong, nonatomic)NSString *goodsName;
+@property(strong, nonatomic)NSString *imageUrl;
+
+@end
---
+++ yunmei.967067/GoodsModel.m
@@ -0,0 +1,18 @@
+//
+//  GoodsModel.m
+//  yunmei.967067
+//
+//  Created by bevin chen on 12-11-29.
+//  Copyright (c) 2012年 bevin chen. All rights reserved.
+//
+
+#import "GoodsModel.h"
+
+@implementation GoodsModel
+
+@synthesize goodsId;
+@synthesize goodsPrice;
+@synthesize goodsName;
+@synthesize imageUrl;
+
+@end
