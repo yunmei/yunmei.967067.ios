a08afebae86bd49ddc8639bd29c7700de31f7472
bevin1984 <bevin1984@gmail.com>
2012-12-05 15:26:41 +0800

    添加GoodsSearchViewController

---
+++ yunmei.967067/GoodsSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoodsSearchViewController.h
+//  yunmei.967067
+//
+//  Created by bevin chen on 12-12-5.
+//  Copyright (c) 2012年 bevin chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoodsSearchViewController : UIViewController
+
+@end
