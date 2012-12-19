7e104ea8b40aea2666f49e1bacc09fe63347692b
wanghong <89240220@qq.com>
2012-12-19 10:24:38 +0800

    加入商品介绍GoodsIntroductViewController这个控制器

---
+++ yunmei.967067/GoodsIntroductViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoodsIntroductViewController.h
+//  yunmei.967067
+//
+//  Created by ken on 12-12-19.
+//  Copyright (c) 2012年 bevin chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoodsIntroductViewController : UIViewController
+
+@end
---
+++ yunmei.967067/GoodsIntroductViewController.m
@@ -0,0 +1,38 @@
+//
+//  GoodsIntroductViewController.m
+//  yunmei.967067
+//
+//  Created by ken on 12-12-19.
+//  Copyright (c) 2012年 bevin chen. All rights reserved.
+//
+
+#import "GoodsIntroductViewController.h"
+
+@interface GoodsIntroductViewController ()
+
+@end
+
+@implementation GoodsIntroductViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
