ad7754990508bbcc04d77c0d17949edf1efd5f29
wanghong <89240220@qq.com>
2013-01-10 08:59:25 +0800

    为订单添加地址页面

---
+++ yunmei.967067/AddAdressViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddAdressViewController.h
+//  yunmei.967067
+//
+//  Created by ken on 13-1-10.
+//  Copyright (c) 2013年 bevin chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddAdressViewController : UIViewController
+
+@end
---
+++ yunmei.967067/AddAdressViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddAdressViewController.m
+//  yunmei.967067
+//
+//  Created by ken on 13-1-10.
+//  Copyright (c) 2013年 bevin chen. All rights reserved.
+//
+
+#import "AddAdressViewController.h"
+
+@interface AddAdressViewController ()
+
+@end
+
+@implementation AddAdressViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
