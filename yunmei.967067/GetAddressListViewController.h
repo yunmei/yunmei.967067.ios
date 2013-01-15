2cec812ccfd4b27d75ae96ff9016cde1af021f41
wanghong <89240220@qq.com>
2013-01-15 14:58:10 +0800

    加入用户地址列表页

---
+++ yunmei.967067/GetAddressListViewController.h
@@ -0,0 +1,13 @@
+//
+//  GetAddressListViewController.h
+//  yunmei.967067
+//
+//  Created by ken on 13-1-15.
+//  Copyright (c) 2013年 bevin chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GetAddressListViewController : UIViewController
+
+@end
---
+++ yunmei.967067/GetAddressListViewController.m
@@ -0,0 +1,38 @@
+//
+//  GetAddressListViewController.m
+//  yunmei.967067
+//
+//  Created by ken on 13-1-15.
+//  Copyright (c) 2013年 bevin chen. All rights reserved.
+//
+
+#import "GetAddressListViewController.h"
+
+@interface GetAddressListViewController ()
+
+@end
+
+@implementation GetAddressListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
